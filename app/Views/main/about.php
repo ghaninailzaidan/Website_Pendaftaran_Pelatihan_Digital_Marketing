0faae09c4907862be95afc8c9b21ebc6b332a14e
Ghani Nail Zaidan <ghaninailzaidan@gmail.com>
2023-07-04 23:21:47 +0700

    Website_Pendaftaran_Pelatihan_Digital_Marketing

---
+++ app/Views/main/about.php
@@ -0,0 +1,86 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<div class="info">
+				<center>
+					<h2><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;Tentang Pelatihan "Digital Marketing"</h2>
+				</center>
+				<hr style="border: 1px grey solid;">
+			</div>
+			<div class="info-content">
+				<p style="text-align:justify;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					LKP HANDAYANI Bekerja Sama Dengan DIREKTORAT KURSUS DAN PELATIHAN KEMENDIKBUD Menyelenggarakan program pendidikan kecakapan Wirausaha "DIGITAL MARKETING".
+					Pelatihan ini terbuka untuk umum.
+					Informasi lengkap mengenai pelatihan ini sebagai berikut :
+				</p>
+				<table width="100%">
+					<tr>
+						<td class="kotak" width="25%" valign="top">
+							<center>
+								<p class="title"><i class="fas fa-users"></i>&nbsp;&nbsp;Keynote Speaker :</p>
+							</center>
+							<div class="inner-content">
+								<p>
+									Dr. Basuki Rahmat, S.Si., M.T.
+								</p>
+								<br>
+							</div>
+						</td>
+						<td class="kotak" width="25%" valign="top">
+							<center>
+								<p class="title"><i class="far fa-clock"></i>&nbsp;&nbsp;Time :</p>
+							</center>
+							<div class="inner-content">
+								<p>
+									Sabtu, 10 Mei 2023<br>
+									(Pukul 09.30 WIB - Selesai)
+								</p>
+								<br>
+							</div>
+						</td>
+						<td class="kotak" width="25%" valign="top">
+							<center>
+								<p class="title"><i class="fas fa-map-marker-alt"></i>&nbsp;&nbsp;Venue :</p>
+							</center>
+							<div class="inner-content">
+								<p>
+									Ruang Pelatihan, LPK HANDAYANI
+								</p>
+								<br>
+							</div>
+						</td>
+						<td class=" kotak" width="25%" valign="top">
+							<center>
+								<p class="title"><i class="fas fa-phone-alt"></i>&nbsp;&nbsp;Contact Person :</p>
+							</center>
+							<div class="inner-content">
+								<p>
+									Ghani (+62 896-5235-3538)
+								</p>
+								<br>
+							</div>
+						</td>
+					</tr>
+				</table>
+				<table width="100%">
+					<tr>
+						<td class="kotak-footer" width="50%" valign="top">
+							<center>
+								<p class="title"><i class="fas fa-cloud-download-alt"></i>&nbsp;&nbsp;Download Poster :</p>
+							</center>
+							<hr style="border: 1px grey solid;">
+							<div class="inner-content">
+								<p>Klik tombol dibawah ini untuk men-download..</p>
+								<a type="button" class="btn btn-info btn-sm" href="https://drive.google.com/file/d/1SC-WtxPe-FPbIcNNLOYeZhfwsbJKfFrP/view?usp=sharing"><i class="fas fa-download"></i>&nbsp;&nbsp;Download Poster</a>
+							</div>
+						</td>
+					</tr>
+				</table>
+				<br><br>
+			</div>
+		</div>
+
+		<?= $this->endSection(); ?>
---
+++ app/Views/main/home.php
@@ -0,0 +1,20 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<div class="konten">
+				<img class="pict1" src="<?= base_url('img/analysis.png'); ?>">
+			</div>
+			<center>
+				<h3>Selamat Datang Di Website Pendaftaran Pelatihan "Digital Marketing"</h3>
+				<p>Terbuka Untuk Umum - Penyelenggara : LKP HANDAYANI<b>
+						<!-- <span style="color:yellow;">*event fiktif</span></b> -->
+						<p>
+			</center>
+		</div>
+	</div>
+</div>
+
+<?= $this->endSection(); ?>
